12be17acb39f0a30b6136f832b1170ff302fdd0e
Trang <trangology@gmail.com>
2021-08-09 17:38:58 +0300

    fix getCourse method

--- src/main/java/com/example/courseapidata/course/CourseController.java
+++ src/main/java/com/example/courseapidata/course/CourseController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class CourseController {
@@ -12,14 +13,14 @@ public class CourseController {
     @Autowired
     private CourseService courseService;
 
-    @RequestMapping("/topics/{id}/courses/")
+    @RequestMapping("/topics/{id}/courses")
     public List<Course> getAllCourses(@PathVariable String id) {
         return courseService.getAllCourses(id);
     }
 
     @RequestMapping("/topics/{topicId}/courses/{id}")
-    public void getCourse(@PathVariable String id) {
-        courseService.getCourse(id);
+    public Optional<Course> getCourse(@PathVariable String id) {
+        return courseService.getCourse(id);
     }
 
     @RequestMapping(method = RequestMethod.POST, value = "/topics/{topicId}/courses/{id}")
--- src/main/java/com/example/courseapidata/course/CourseService.java
+++ src/main/java/com/example/courseapidata/course/CourseService.java
@@ -4,8 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CourseService {
@@ -18,8 +18,8 @@ public class CourseService {
         return courses;
     }
 
-    public void getCourse(String id) {
-        courseRepository.findById(id);
+    public Optional<Course> getCourse(String id) {
+        return courseRepository.findById(id);
     }
 
     public void addCourse(Course course) {
